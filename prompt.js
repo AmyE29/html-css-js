9bd699f77ac68941e21e2d0c20f04cf11a7f46cc
Amy Evans <amykarline@yahoo.com>
2019-09-18 21:23:58 -0700

    added js coding to index

---
+++ prompt.js
@@ -0,0 +1,12 @@
+var answer = prompt ('Do you drink water?');
+
+var message;
+if (answer === 'yes') {
+    message = 'You need a water bottle!';
+} else if (answer === 'no'){ 
+    message = 'OMG! youre going to die without a water bottle!';
+} else {
+    message = 'Drink more water!';
+}
+
+document.write('<h5>'+ message + '</h5>');
