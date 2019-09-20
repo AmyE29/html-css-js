aa619b474d14114b9f911b1e34b8ada69c5ead03
Amy Evans <amykarline@yahoo.com>
2019-09-19 20:16:01 -0700

    fixed prompt bug

--- extra.js
+++ extra.js
@@ -5,7 +5,7 @@ if (need_water === 'yes') {
     intro = 'You need a better one!';
 }
 else {
-   intro = 'Youve come to the right place!';
+   intro = 'You\'ve come to the right place!';
 }
 
 document.write('<h5>' + intro + '</h5>');
