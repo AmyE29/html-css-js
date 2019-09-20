96c5d831ea0ab7cb29226d5e0f47eff644863edb
Amy Evans <55407183+AmyE29@users.noreply.github.com>
2019-09-20 08:24:21 -0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -4,21 +4,17 @@ function askName()  {
   }
   
   function drinkWater() {
-    var askHowMuch = confirm('Do you have a water bottle?');
+    var withWhat = confirm('Do you have a water bottle?');
     var bottle = '<img src="http://giphygifs.s3.amazonaws.com/media/12IK01JJiBXtvi/giphy.gif">';
-    if (askHowMuch === true) {
-        var howMany = '';
-        while (Number.isInteger(howMany) === false) {
-            howMany = Number(prompt('How water botles would you like?'));
+    if (withWhat === true) {
+        var count = '';
+        while (Number(count) === false) {
+            count = Number(prompt('How many water bottles would you like?'));
         }
             var result = '';
             for (i=0; i < (howMany); i++) {
-                count = i + 1;
-                result = '<div>' + result + count + bottle + '</div>';  
+                realCount = i + 1;
+                result = '<p>' + result + realCount + bottle + '</p>';  
             }
             return result; 
     }
-    else {
-        return 'You may want drink more water.';
-    } 
-  }
