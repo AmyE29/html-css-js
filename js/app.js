4fe28d9b2f3b4a613f334575429cb0794f2434c9
Amy Evans <amykarline@yahoo.com>
2019-09-20 10:33:33 -0700

    change opening greeting to a function

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ function askName()  {
   }
   
   function drinkWater() {
-    var askWithWhat = confirm('Do you have a water bottle?');
+    var askWithWhat = confirm('Do you need a water bottle?');
     var bottle = '<img src="http://giphygifs.s3.amazonaws.com/media/12IK01JJiBXtvi/giphy.gif">';
     if (askWithWhat === true) 
         var howMany = prompt ('How water bottles would you like?') ;
@@ -18,3 +18,20 @@ function askName()  {
             }
             return result; 
     }
+    function getTime() {
+      var today = new Date();
+      var hourNow = today.getHours();
+      var greeting;
+      
+      if (hourNow > 18) {
+          greeting = 'Remember to Hydrate at Night!';
+      } else if (hourNow > 12) {
+          greeting = 'Time for your afternoon hydration!';
+      } else if (hourNow > 0) {
+          greeting = 'Remember to drink 64oz a day!';
+      } else {
+          greeting = 'Water is Life!';
+      }
+      
+      return '<h5>'+ greeting + '</h5>';
+      }
